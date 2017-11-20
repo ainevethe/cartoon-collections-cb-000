3b5d8173dda27096f08bf7e1bb3fd9ac6a643c51
Crista Cooper <aine3d@gmail.com>
2017-11-20 02:04:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_array)
-  calls_array.include? {|a| a.length < 5}
+  calls_array.count { |call| call.length < 4}  
 end
 
 def find_the_cheese# code an argument here
